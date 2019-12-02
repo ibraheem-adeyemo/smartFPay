9d4b4f15f9918e578dcfbaeae6496814e61faf84
esther.akinloose <esther.akinloose@interswitchng.com>
2019-12-02 16:04:38 +0100

    layout fix

--- src/shared/layouts/MainLayout/MainLayout.tsx
+++ src/shared/layouts/MainLayout/MainLayout.tsx
@@ -8,13 +8,13 @@ import './MainLayout.css'
 export const MainLayout = ({component: Component, ...rest}) => (
     <Route {...rest} render={props => (
         <>
-            <Navbar />
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-md-2 d-none d-md-block bg-light sidebar">
                         <DashboardSideBar />
                     </div>
                     <div className="col-md-9 ml-sm-auto col-lg-10 px-4 component">
+                        <Navbar />
                         <Component {...props} />
                     </div>
                 </div>
