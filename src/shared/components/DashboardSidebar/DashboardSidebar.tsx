0008d7941f953dd285e3bc4bc9fc2f5a10aea4ed
Esther Akinloose <esther.akinloose@Interswitchng.com>
2020-01-30 15:05:42 +0000

    Dashboard Sidebar fix

--- src/shared/components/DashboardSidebar/DashboardSidebar.tsx
+++ src/shared/components/DashboardSidebar/DashboardSidebar.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './DashboardSidebar.css';
 import {NavLink} from 'react-router-dom';
 
 let toggleSubMenu = (event: React.FormEvent<any>) => {
     const subMenuItem = event.currentTarget;
-    const container = document.querySelectorAll('.Sidebar-wrapper .sub-nav');
-    if(!subMenuItem.querySelector('ul').classList.contains('closed')) {
-        console.log(!subMenuItem.querySelector('ul').classList.contains('closed'))
-        subMenuItem.querySelector('ul').classList.add('closed')
-    }
-    console.log(subMenuItem.querySelector('ul.sub-nav').classList)
-    Array.from(container).forEach((item) => {
-        item.classList.add('closed');
-    });
-    subMenuItem.querySelector('ul').classList.remove('closed')
-    
-    console.log(subMenuItem.querySelector('ul.sub-nav').classList)
     // TODO: Fix ToggleToClose
 };
 
 const DashboardSideBar = () => {
+    const [showCustomerManagementChildren, setShowCustomerManagementChildren] = useState(false);
+    const [showAdminManagementChildren, setShowAdminManagementChildren] = useState(false);
+    const [showLimitManagementChildren, setShowLimitManagementChildren] = useState(false);
+    const [showSystemManagementChildren, setShowSystemManagementChildren] = useState(false);
+    const [showReportManagementChildren, setShowReportManagementChildren] = useState(false);
+
+    const toggleCustomerManagement = () => {
+        setShowCustomerManagementChildren(!showCustomerManagementChildren);
+        setShowAdminManagementChildren(false);
+        setShowLimitManagementChildren(false);
+        setShowReportManagementChildren(false);
+        setShowSystemManagementChildren(false);
+    }
+
+    const toggleAdminManagement = () => {
+        setShowAdminManagementChildren(!showAdminManagementChildren);
+        setShowCustomerManagementChildren(false);
+        setShowLimitManagementChildren(false);
+        setShowReportManagementChildren(false);
+        setShowSystemManagementChildren(false);
+    }
+
+    const toggleLimitManagement = () => {
+        setShowLimitManagementChildren(!showLimitManagementChildren);
+        setShowAdminManagementChildren(false);
+        setShowCustomerManagementChildren(false);
+        setShowReportManagementChildren(false);
+        setShowSystemManagementChildren(false);
+    }
+
+    const toggleSystemManagement = () => {
+        setShowSystemManagementChildren(!showSystemManagementChildren);
+        setShowAdminManagementChildren(false);
+        setShowLimitManagementChildren(false);
+        setShowReportManagementChildren(false);
+        setShowCustomerManagementChildren(false);
+    }
+
+    const toggleReportManagement = () => {
+        setShowReportManagementChildren(!showReportManagementChildren);
+        setShowAdminManagementChildren(false);
+        setShowLimitManagementChildren(false);
+        setShowCustomerManagementChildren(false);
+        setShowSystemManagementChildren(false);
+    }
+
     return (
         <div className="sidebar-sticky">
             <ul className="list list-unstyled nav">
@@ -31,46 +65,46 @@ const DashboardSideBar = () => {
                         <i className="icon-dashboard"/> Dashboard
                     </NavLink>
                 </li>
-                <li onClick={toggleSubMenu}>
-                    <a>Customer Management <span className="icon"><i className="fa fa-caret-down" aria-hidden="true"></i></span></a> 
-                    <ul className="sub-nav list-unstyled closed">
+                <li onClick={toggleCustomerManagement}>
+                    <a>Customer Management <span className="icon"><i className={showCustomerManagementChildren?"fa fa-caret-down":"fa fa-caret-up"} aria-hidden="true"></i></span></a> 
+                    {showCustomerManagementChildren && <ul className="sub-nav list-unstyled">
                         <li><NavLink to="/" activeClassName="active">Sublink</NavLink></li>
                         <li><NavLink to="/">SubLink</NavLink></li>
                         <li><NavLink to="/" activeClassName="active">Sublink </NavLink></li>
-                    </ul>
+                    </ul>}
                 </li>
 
-                <li onClick={toggleSubMenu}>
-                    <a>Admin Management <span className="icon"><i className="fa fa-caret-down" aria-hidden="true"></i></span></a>
-                    <ul className="sub-nav list-unstyled closed">
+                <li onClick={toggleAdminManagement}>
+                    <a>Admin Management <span className="icon"><i className={showAdminManagementChildren?"fa fa-caret-down":"fa fa-caret-up"} aria-hidden="true"></i></span></a>
+                    {showAdminManagementChildren && <ul className="sub-nav list-unstyled">
                         <li><NavLink to="/users" activeClassName="active">Admin Users</NavLink></li>
                         <li><NavLink to="/roles" activeClassName="active">Roles </NavLink></li>
-                    </ul>
+                    </ul>}
                 </li>
-                <li onClick={toggleSubMenu}>
-                    <a>Limit Management <span className="icon"><i className="fa fa-caret-down" aria-hidden="true"></i></span></a>
-                    <ul className="sub-nav list-unstyled closed">
+                <li onClick={toggleLimitManagement}>
+                    <a>Limit Management <span className="icon"><i className={showLimitManagementChildren?"fa fa-caret-down":"fa fa-caret-up"} aria-hidden="true"></i></span></a>
+                    {showLimitManagementChildren && <ul className="sub-nav list-unstyled">
                         <li><NavLink to="/" activeClassName="active">Sublink</NavLink></li>
                         <li><NavLink to="/">SubLink</NavLink></li>
                         <li><NavLink to="/" activeClassName="active">Sublink </NavLink></li>
-                    </ul>
+                    </ul>}
                 </li>
 
-                <li onClick={toggleSubMenu}>
-                    <a>System Management <span className="icon"><i className="fa fa-caret-down" aria-hidden="true"></i></span></a>
-                    <ul className="sub-nav list-unstyled closed">
+                <li onClick={toggleSystemManagement}>
+                    <a>System Management <span className="icon"><i className={showSystemManagementChildren?"fa fa-caret-down":"fa fa-caret-up"} aria-hidden="true"></i></span></a>
+                    {showSystemManagementChildren && <ul className="sub-nav list-unstyled">
                         <li><NavLink to="/" activeClassName="active">Sublink</NavLink></li>
                         <li><NavLink to="/">SubLink</NavLink></li>
                         <li><NavLink to="/" activeClassName="active">Sublink </NavLink></li>
-                    </ul>
+                    </ul>}
                 </li>
-                <li onClick={toggleSubMenu}>
-                    <a>Report Management <span className="icon"><i className="fa fa-caret-down" aria-hidden="true"></i></span></a>
-                    <ul className="sub-nav list-unstyled closed">
+                <li onClick={toggleReportManagement}>
+                    <a>Report Management <span className="icon"><i className={showReportManagementChildren?"fa fa-caret-down":"fa fa-caret-up"} aria-hidden="true"></i></span></a>
+                    {showReportManagementChildren && <ul className="sub-nav list-unstyled">
                         <li><NavLink to="/" activeClassName="active">Sublink</NavLink></li>
                         <li><NavLink to="/">SubLink</NavLink></li>
                         <li><NavLink to="/" activeClassName="active">Sublink </NavLink></li>
-                    </ul>
+                    </ul>}
                 </li>
             </ul>
         </div>
