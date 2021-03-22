7f5eb08c20d55bfbf6bb898a075bc68a02f1b6f9
Johnson Ubaezuonu <johnson@initshq.com>
2021-03-22 10:40:57 +0100

    fixed pagination responsiveness

--- src/shared/components/DataTable/Paginator.js
+++ src/shared/components/DataTable/Paginator.js
@@ -26,74 +26,57 @@ class Paginator extends Component {
     } = this.props;
 
     return (
-      <Row className="my-3 form">
-        <Col sm="3">
-          <Button
-            size="sm"
-            id="previous-page"
-            className="icon"
-            disabled={pageNumber <= 1}
-            onClick={prev}
-            outline
+      <Row className="paginator">
+        <Button
+          id="previous-page"
+          className="paginator-control"
+          disabled={pageNumber <= 1}
+          onClick={prev}
+          outline
+        >
+          <MdChevronLeft />
+          Previous
+        </Button>
+        <div className="page-desc">
+          <div className="">Page</div>
+          <input
+            value={pageNumber}
+            onChange={this.handlePageNumberChange}
+            className="page-input"
+            type="number"
+          />
+          <div
+            className=""
+            style={{ minWidth: "30%" }}
           >
-            <MdChevronLeft />
-            Previous
-          </Button>
-        </Col>
-        <Col sm="6" className="text-center">
-          <Row>
-            <Col md="6">
-              <div className="form__form-group">
-                <div className="form__form-group-field">
-                  <div className="form__form-group-icon">Page</div>
-                  <input
-                    value={pageNumber}
-                    onChange={this.handlePageNumberChange}
-                    className="text-center"
-                    type="number"
-                  />
-                  <div
-                    className="form__form-group-icon"
-                    style={{ minWidth: "30%" }}
-                  >
-                    of {numberOfPages}
-                  </div>
-                </div>
-              </div>
-            </Col>
-            <Col md="6">
-              <div className="form__form-group">
-                <div className="form__form-group-field">
-                  <div className="form__form-group-icon">Show</div>
-                  <select
-                    value={recordsPerPage}
-                    onChange={this.handlePageSizeChange}
-                    name="select"
-                    id="rowcount"
-                  >
-                    {PAGE_SIZE_OPTIONS.map((option, index) => (
-                      <option key={`option${index}`}>{option}</option>
-                    ))}
-                  </select>
-                  <div className="form__form-group-icon">records</div>
-                </div>
-              </div>
-            </Col>
-          </Row>
-        </Col>
-        <Col sm="3" className="text-right">
-          <Button
-            size="sm"
-            className="icon"
-            disabled={pageNumber >= numberOfPages}
-            onClick={next}
-            outline
-            id="next-page"
+            of {numberOfPages}
+          </div>
+        </div>
+        <div className="page-desc">
+          <div className="">Show</div>
+          <select
+            value={recordsPerPage}
+            onChange={this.handlePageSizeChange}
+            name="select"
+            id="rowcount"
+            className="page-input select"
           >
-            Next
-            <MdChevronRight />
-          </Button>
-        </Col>
+            {PAGE_SIZE_OPTIONS.map((option, index) => (
+              <option key={`option${index}`}>{option}</option>
+            ))}
+          </select>
+          <div className="">records</div>
+        </div>
+        <Button
+          className="paginator-control"
+          disabled={pageNumber >= numberOfPages}
+          onClick={next}
+          outline
+          id="next-page"
+        >
+          Next
+          <MdChevronRight />
+        </Button>
       </Row>
     );
   }
